2a3cf4df88aa28c3560407c507746371af717474
Abu Kalam <akwebdev69@gmail.com>
2025-01-24 14:45:02 +0600

    erro error error!

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,41 +1,63 @@
-
 import Link from "next/link";
-import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const Nav = async () => {
-    const { isAuthenticated, getUser } = await getKindeServerSession();
+    const { isAuthenticated, getUser } = getKindeServerSession();
     const isUserAuthenticated = await isAuthenticated();
-    const user = await getUser();
+    const user = isUserAuthenticated ? await getUser() : null;
 
     return (
         <div className="w-full bg-gradient-to-b from-gray-800 to-gray-900">
+            <ul className="flex gap-4 p-4">
+                {/* Home Link */}
+                <li>
+                    <Link href="/" className="text-white hover:bg-orange-700 p-2 rounded-lg">
+                        Home
+                    </Link>
+                </li>
 
-            <li>
-                <Link href={"/"} className="text-white hover:bg-orange-700 p-2 rounded-lg">
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link href={"/dashboard"} className="text-white hover:bg-orange-700 p-2 rounded-lg">
-                    Profile
-                </Link>
-            </li>
-            <li>
-                <RegisterLink>
-                    <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
-                        Sign Up
-                    </button>
-                </RegisterLink>
-            </li>
-            <li>
-                <LoginLink>
-                    <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
-                        Sign In
-                    </button>
-                </LoginLink>
-            </li>
+                {/* Profile Link */}
+                <li>
+                    <Link href="/dashboard" className="text-white hover:bg-orange-700 p-2 rounded-lg">
+                        Profile
+                    </Link>
+                </li>
 
+                {/* Authenticated User View */}
+                {isUserAuthenticated ? (
+                    <>
+                        <li>
+                            <span className="text-white">Welcome, {user?.name}</span>
+                        </li>
+                        <li>
+                            <LogoutLink>
+                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                    Logout
+                                </button>
+                            </LogoutLink>
+                        </li>
+                    </>
+                ) : (
+                    // Guest User View
+                    <>
+                        <li>
+                            <RegisterLink>
+                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                    Sign Up
+                                </button>
+                            </RegisterLink>
+                        </li>
+                        <li>
+                            <LoginLink>
+                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                    Sign In
+                                </button>
+                            </LoginLink>
+                        </li>
+                    </>
+                )}
+            </ul>
         </div>
     );
 };
