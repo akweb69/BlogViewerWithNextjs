148d7b6e6d8bc90a0af0989df3b8580d8fd570eb
Abu Kalam <akwebdev69@gmail.com>
2025-01-24 10:45:26 +0600

    fix navbar conditional button rendering dssd

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";  // Import useState hook
 import Link from "next/link";
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
