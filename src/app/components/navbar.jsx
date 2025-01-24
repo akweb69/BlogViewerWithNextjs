552e2bba93d2322be2e3423a7515ad51cd386df3
Abu Kalam <akwebdev69@gmail.com>
2025-01-24 14:51:05 +0600

    Alhamdulillah!

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -9,55 +9,58 @@ const Nav = async () => {
 
     return (
         <div className="w-full bg-gradient-to-b from-gray-800 to-gray-900">
-            <ul className="flex gap-4 p-4">
-                {/* Home Link */}
-                <li>
-                    <Link href="/" className="text-white hover:bg-orange-700 p-2 rounded-lg">
-                        Home
-                    </Link>
-                </li>
+            <div className="flex w-10/12 mx-auto items-center justify-between flex-wrap ">
+                <div className="text-2xl md:text-4xl font-bold text-teal-200">Blogs</div>
+                <div className="">
+                    <ul className="flex gap-4 py-4">
+                        {/* Home Link */}
+                        <li>
+                            <Link href="/" className="text-white hover:bg-orange-700 p-2 rounded-lg">
+                                Home
+                            </Link>
+                        </li>
 
-                {/* Profile Link */}
-                <li>
-                    <Link href="/dashboard" className="text-white hover:bg-orange-700 p-2 rounded-lg">
-                        Profile
-                    </Link>
-                </li>
+                        {/* Profile Link */}
+                        <li>
+                            <Link href="/dashboard" className="text-white hover:bg-orange-700 p-2 rounded-lg">
+                                Profile
+                            </Link>
+                        </li>
 
-                {/* Authenticated User View */}
-                {isUserAuthenticated ? (
-                    <>
-                        <li>
-                            <span className="text-white">Welcome, {user?.name}</span>
-                        </li>
-                        <li>
-                            <LogoutLink>
-                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
-                                    Logout
-                                </button>
-                            </LogoutLink>
-                        </li>
-                    </>
-                ) : (
-                    // Guest User View
-                    <>
-                        <li>
-                            <RegisterLink>
-                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
-                                    Sign Up
-                                </button>
-                            </RegisterLink>
-                        </li>
-                        <li>
-                            <LoginLink>
-                                <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
-                                    Sign In
-                                </button>
-                            </LoginLink>
-                        </li>
-                    </>
-                )}
-            </ul>
+                        {/* Authenticated User View */}
+                        {isUserAuthenticated ? (
+                            <>
+
+                                <li>
+                                    <LogoutLink>
+                                        <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                            Logout
+                                        </button>
+                                    </LogoutLink>
+                                </li>
+                            </>
+                        ) : (
+                            // Guest User View
+                            <>
+                                <li>
+                                    <RegisterLink>
+                                        <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                            Sign Up
+                                        </button>
+                                    </RegisterLink>
+                                </li>
+                                <li>
+                                    <LoginLink>
+                                        <button className="p-2 px-4 text-white border rounded-lg hover:bg-white hover:text-orange-500 transition">
+                                            Sign In
+                                        </button>
+                                    </LoginLink>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
